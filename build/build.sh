1c350f11f5ed842a553f9a742065e14187d1008c
Dan Brown <ssddanbrown@googlemail.com>
2016-04-10 15:09:41 +0100

    Added osx app bundler to script

--- build/build.sh
+++ build/build.sh
@@ -2,13 +2,17 @@
 #
 rice embed-go
 
+# Set correct script dir
+DIR="$(cd "$(dirname "$0")" && pwd)"
+cd $DIR/..
+
 if [ "$1" != "all" ]
 then
 	# Create only locally
 	go install github.com/ssddanbrown/webby
 else
 	echo "Building binaries for all platforms"
-	COMMAND="go build -o bins/webby"
+	COMMAND="go build -o build/bins/webby"
 
 	eval "env GOOS=windows GOARCH=amd64 $COMMAND-windows-amd64.exe"
 	echo "Windows binary built"
@@ -16,7 +20,12 @@ else
 	eval "env GOOS=linux GOARCH=amd64 $COMMAND-linux-amd64"
 	echo "Linux binary built"
 
+	# Create OSX app bundle
+	rm -rf build/bins/webby-osx.app
+	mkdir build/bins/webby-osx.app
 	eval "env GOOS=darwin GOARCH=amd64 $COMMAND-darwin-amd64"
+	mv build/bins/webby-darwin-amd64 build/bins/webby-osx.app/webby
+	cp build/Info.plist build/bins/webby-osx.app/Info.plist
 	echo "OSX binary built"
 fi
 
